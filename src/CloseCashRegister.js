dd7ec3c61f4a7e36318ebd19e4ab10a6ed58af24
Juan Carlos Garcia Equivel <carlos.garcia.esquivel@live.com.mx>
2020-06-19 18:34:41 -0500

    add initial styles

--- src/CloseCashRegister.js
+++ src/CloseCashRegister.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { URL_STORE_OPEN_REGISTER, URL_STORE_CLOSE_REGISTER } from './constants';
+import { URL_STORE_CLOSE_REGISTER } from './constants';
 
 class CloseCashRegister extends React.Component {
 
@@ -26,49 +26,52 @@ class CloseCashRegister extends React.Component {
     }
 
     render() {
-        return <form action="" method="POST" className='form' onSubmit={ (event)=> this.handleSubmit(event) }>
-            <div className="form-group">
-                <label htmlFor="closing_date">Fecha de Cierre</label>
-                <input type="date" 
-                    name="closing_date" 
-                    id="closing_date" 
-                    required
-                    
-                    />
+        return <>
+            <h3>Formulario de cierre de caja</h3>
+            <form action="" method="POST" className='form' onSubmit={ (event)=> this.handleSubmit(event) }>
+                <div className="form-group">
+                    <label htmlFor="closing_date">Fecha de Cierre</label>
+                    <input type="date" 
+                        name="closing_date" 
+                        id="closing_date" 
+                        required
+                        
+                        />
+                        <span className='feedback'></span>
+                </div>
+                <div className="form-group">
+                    <label htmlFor="closing_hour">Hora de Cierre</label>
+                    <input type="time" name="closing_hour" id="closing_hour" required  />
                     <span className='feedback'></span>
-            </div>
-            <div className="form-group">
-                <label htmlFor="closing_hour">Hora de Cierre</label>
-                <input type="time" name="closing_hour" id="closing_hour" required  />
-                <span className='feedback'></span>
-            </div>
+                </div>
 
-            <div className="form-group">
-                <label htmlFor="card_value">Ventas Tarjeta</label>
-                <input type="numeric" name="card_value" id="card_value" required  />
-                <span className='feedback'></span>
-            </div>
+                <div className="form-group">
+                    <label htmlFor="card_value">Ventas Tarjeta</label>
+                    <input type="number" name="card_value" id="card_value" required  />
+                    <span className='feedback'></span>
+                </div>
 
-            <div className="form-group">
-                <label htmlFor="cash_value">Ventas Efectivo</label>
-                <input type="numeric" name="cash_value" id="cash_value" required   />
-                <span className='feedback'></span>
-            </div>
+                <div className="form-group">
+                    <label htmlFor="cash_value">Ventas Efectivo</label>
+                    <input type="number" name="cash_value" id="cash_value" min="0" step="1" required   />
+                    <span className='feedback'></span>
+                </div>
 
-            <div className="form-group">
-                <label htmlFor="sales_value">Total Ventas</label>
-                <input type="numeric" name="sales_value" id="sales_value" required   />
-                <span className='feedback'></span>
-            </div>
+                <div className="form-group">
+                    <label htmlFor="sales_value">Total Ventas</label>
+                    <input type="number" name="sales_value" id="sales_value" min="0" step="1" required   />
+                    <span className='feedback'></span>
+                </div>
 
-            <div className="form-group">
-                <label htmlFor="closing_value">Total en caja</label>
-                <input type="numeric" name="closing_value" id="closing_value" required   />
-                <span className='feedback'></span>
-            </div>
-         
-            <input type="submit" value="Cerrar caja"/>
-        </form>;
+                <div className="form-group">
+                    <label htmlFor="closing_value">Total en caja</label>
+                    <input type="number" name="closing_value" id="closing_value" min="0" step="1" required   />
+                    <span className='feedback'></span>
+                </div>
+            
+                <input type="submit" value="Cerrar caja"/>
+            </form>;
+        </>
     }
 }
 
--- src/OpenCashRegister.js
+++ src/OpenCashRegister.js
@@ -18,33 +18,36 @@ class OpenCashRegister extends React.Component {
     }
 
     render() {
-        return <form action="" method="POST" className='form' onSubmit={ (event)=> this.handleSubmit(event) }>
-            <div className="form-group">
-                <label htmlFor="opening_date">Fecha de Apertura</label>
-                <input type="date" 
-                    name="opening_date" 
-                    id="opening_date" 
-                    required
-                    />
+        return <>
+            <h3>Formulario de apertura de caja</h3>
+            <form action="" method="POST" className='form' onSubmit={ (event)=> this.handleSubmit(event) }>
+                <div className="form-group">
+                    <label htmlFor="opening_date">Fecha de Apertura</label>
+                    <input type="date" 
+                        name="opening_date" 
+                        id="opening_date" 
+                        required
+                        />
+                        <span className='feedback'></span>
+                </div>
+                <div className="form-group">
+                    <label htmlFor="opening_hour">Hora de Apertura</label>
+                    <input type="time" name="opening_hour" id="opening_hour" required />
                     <span className='feedback'></span>
-            </div>
-            <div className="form-group">
-                <label htmlFor="opening_hour">Hora de Apertura</label>
-                <input type="time" name="opening_hour" id="opening_hour" required />
-                <span className='feedback'></span>
-            </div>
-            <div className="form-group">
-                <label htmlFor="previous_closing_value">Cierre anterior</label>
-                <input type="number" min="0" name="previous_closing_value" id="previous_closing_value" required />
-                <span className='feedback'></span>
-            </div>
-            <div className="form-group">
-                <label htmlFor="opening_value">Base</label>
-                <input type="number" min="0" name="opening_value" id="opening_value" required />
-                <span className='feedback'></span>
-            </div>
-            <input type="submit" value="Abrir caja"/>
-        </form>;
+                </div>
+                <div className="form-group">
+                    <label htmlFor="previous_closing_value">Cierre anterior</label>
+                    <input type="number" min="0" name="previous_closing_value" id="previous_closing_value" required />
+                    <span className='feedback'></span>
+                </div>
+                <div className="form-group">
+                    <label htmlFor="opening_value">Base</label>
+                    <input type="number" min="0" name="opening_value" id="opening_value" required />
+                    <span className='feedback'></span>
+                </div>
+                <input type="submit" value="Abrir caja"/>
+            </form>
+        </>;
     }
 }
 
